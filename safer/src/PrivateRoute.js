7ff5bdc35ab3b71aa12aa488cd27aadd71744eff
Shiv Shankar Singh <ms18006@iisermohali.ac.in>
2024-02-03 01:25:58 +0530

    update for build warnings

--- safer/src/PrivateRoute.js
+++ safer/src/PrivateRoute.js
@@ -1,7 +1,7 @@
 // src/PrivateRoute.js
 
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
 export default function PrivateRoute({ children }) {
